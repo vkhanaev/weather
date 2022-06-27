59efe7234df081c8837ea9a7ee636829528d2bd8
vkhanaev <vkhanaev@ya.ru>
2022-06-27 12:11:24 +0300

    добавили liveData в observer, когда меняются данные во ViewModel - фрагмент это узнает и отображает изменения

--- app/src/main/java/com/example/weather/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/weather/ui/main/MainFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import com.example.weather.R
 
 class MainFragment : Fragment() {
@@ -23,10 +25,34 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+     // Заменили onActivityCreated на onViewCreated
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Подключили viewModel
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        // Добавили observer, что такое it?
+        // Получили LiveData, вызвали observe
+        //val observer = Observer<Any> { renderData(it) }
+        //val LiveData = viewModel.getData()
+        //LiveData.observe(viewLifecycleOwner, observer)  // Теперь, если данные, которые хранит LiveData, изменятся, Observer сразу об этом узнает и вызовет метод renderData, куда передаст новые данные.
+
+        // Подписываемся, Any - тип объекта в liveDataToObserve
+        viewModel.liveDataToObserve.observe(viewLifecycleOwner, object : Observer<Any> {
+            override fun onChanged(t: Any?) {
+                Toast.makeText(context, "Работает $t", Toast.LENGTH_LONG).show()
+            }
+
+        })
+
+        // Отправляем запрос на обновление данных
+        viewModel.getData()
     }
+    private fun renderData(data: Any) {
+        Toast.makeText(context, "data", Toast.LENGTH_LONG).show()
+    }
+
+
 
 }
